122a929df83ac6f9b2080242d9567cb6978c8a35
Lawrence Cunanan <enzocunanan01@gmail.com>
2026-02-14 09:32:23 +0800

    fix: update tab label from "Your Goals" to "My Goals" in ManageGoalsDialog

--- components/dialogs/goals/manage-dialog.tsx
+++ components/dialogs/goals/manage-dialog.tsx
@@ -232,7 +232,7 @@ export function ManageGoalsDialog({
 				>
 					<TabsList className="grid w-full grid-cols-2 mb-2">
 						<TabsTrigger value="create">Create Goal</TabsTrigger>
-						<TabsTrigger value="manage">Your Goals</TabsTrigger>
+						<TabsTrigger value="manage">My Goals</TabsTrigger>
 					</TabsList>
 
 					{/* Create Goal Tab */}
